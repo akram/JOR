203e5b0e2dce509c111f4105e95f8a1f179acc28
ABDALA Olga <abdala@solucom.fr>
2016-05-09 13:30:58 +0200

    [Olga] Ajout du fichier AjaxTab.js

--- js/AjaxTab.js
+++ js/AjaxTab.js
@@ -56,10 +56,22 @@ function add_entry()
 {
 popup = window.open('', 'popup', 'height=220, width=420');
 popup.document.write('<form action="../Controleur/addContact.php" method="post" onsubmit=self.close();>');
-popup.document.write('<div><label style="display:inline-block;width: 250px;text-align: right;">Name: </label><input type="text" id="Name" name="Name" ></div>');
+popup.document.write('<div><label style="display:inline-block;width: 250px;text-align: right;">Name: </label><input type="text" id="Name" name="Name" /></div>');
 popup.document.write('<div><label style="display:inline-block;width: 250px;text-align: right;">Firt Name: </label><input type="text" id="First_Name" name="First_Name" /></div>');
 popup.document.write('<div><label style="display:inline-block;width: 250px;text-align: right;">Mail: </label><input type="text" id="mail" name="mail" /></div>');
 popup.document.write('<div><label style="display:inline-block;width: 250px;text-align: right;">Tel: </label><input type="text" id="tel" name="tel" /></div>');
 popup.document.write('<div style="margin-left:76%"><input style="text-align:right;display:inline-block;" type="submit" value="Add" onclick="opener.location.reload();" /></div>');
 popup.document.write('</form>');
+}
+
+
+function modify_entry(name,first,mail,tel,id){
+popup = window.open('', 'popup', 'height=220, width=420');
+popup.document.write('<form  action="../Controleur/modifyContact.php?id='+id+'" method="post" onsubmit=self.close();>');
+popup.document.write('<div><label style="display:inline-block;width: 250px;text-align: right;">Name: </label><input type="text" id="Name" name="Name" value="'+name+'"/></div>');
+popup.document.write('<div><label style="display:inline-block;width: 250px;text-align: right;">Firt Name: </label><input type="text" id="First_Name" name="First_Name" value="'+first+'" /></div>');
+popup.document.write('<div><label style="display:inline-block;width: 250px;text-align: right;">Mail: </label><input type="text" id="mail" name="mail" value="'+mail+'"/></div>');
+popup.document.write('<div><label style="display:inline-block;width: 250px;text-align: right;">Tel: </label><input type="text" id="tel" name="tel" value="'+tel+'"/></div>');
+popup.document.write('<div style="margin-left:76%"><input style="text-align:right;display:inline-block;" type="submit" value="Modify" onclick="opener.location.reload();" /></div>');
+popup.document.write('</form>');	
 }
