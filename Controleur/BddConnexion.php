fd52130409bdea2a2b976eeaf7a6d74b049139d4
Solupoc <solupoc@gmail.com>
2016-06-14 15:39:21 +0200

    Update BddConnexion.php
    Hostname modification

--- Controleur/BddConnexion.php
+++ Controleur/BddConnexion.php
@@ -8,7 +8,7 @@ private $bdd;
 	function connect(){
  try { 
 $dbName = 'poc'; 
-$host = 'localhost'; 
+$host = 'db-service'; 
 $utilisateur = 'root';
  $motDePasse = '';
 $dns = 'mysql:host='.$host .';dbname='.$dbName.';';
