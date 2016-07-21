6f6d721acec8a2753a864dfa01a6d01bb5d4602b
Solupoc <solupoc@gmail.com>
2016-07-21 15:53:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
             
         <input name="user_id" type="mail" id="user_id" placeholder="Name" class="form-control" autofocus> 
         <input type="password" name="password" id="password" placeholder="Password" class="form-control" >       
-		<input class="btn btn-lg btn-primary btn-block" type="submit" background-color="#800080" name="Submit" value="Connect">
+		<input class="btn btn-lg btn-primary btn-block" type="submit" background-color=#800080 name="Submit" value="Connect">
 		<input class="btn btn-lg btn-primary btn-block" type="submit" name="Register" value="Create new account">
       </form>
 	
